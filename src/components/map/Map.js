2d48392d9db353c1d58578052b5f6c771f03c27d
ekidhaja <ekidhaja@gmail.com>
2022-04-01 01:06:51 +0100

    Map added and loads

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,31 @@
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+//import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
+
 const Map = () => {
+    const matches = useMediaQuery('(min-width:600px)');
+    const classes = useStyles();
+    const coords = { lat: 0, lng: 0 }
+
     return ( 
-        <div>Map</div>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={coords}
+                center={coords}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                /*options={''}
+                onChange={''}
+                onChildClick={''}*/
+            >
+
+            </GoogleMapReact>
+        </div>
      );
 }
  
